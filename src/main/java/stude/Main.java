98690edc0452edf16c2497b5cb46f538d37a6443
MSI <ekaterinavar1998@mail.ru>
2023-10-10 20:21:06 +0300

    красиво

--- src/main/java/stude/Main.java
+++ src/main/java/stude/Main.java
@@ -11,7 +11,7 @@ public class Main {
             int i = number.nextInt();
             System.out.println("Введенное вами чэисло является целым!");
         } else {
-            System.out.println("Введенное вами число не является це'''лым!");
+            System.out.println("Введенное вами число не является целым!");
         }
     }
 }
