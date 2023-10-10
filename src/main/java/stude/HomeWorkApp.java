1134d7288bc21c00057c4cf12cc172ad808befa0
MSI <ekaterinavar1998@mail.ru>
2023-10-10 20:41:06 +0300

    добавил переменную

--- src/main/java/stude/HomeWorkApp.java
+++ src/main/java/stude/HomeWorkApp.java
@@ -2,6 +2,8 @@ package stude;
 
 public class HomeWorkApp {
     public static void main(String[] args){
-        System.out.println("Спасибо");
+        System.out.println("Спасибо ");
+        int f = 6;
+        System.out.println(f);
     }
 }
